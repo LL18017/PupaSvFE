4057ef736a0c2bcf5e60dddfcc1675d422d2cf53
mjlopez <ll18017@ues.edu.sv>
2025-05-27 11:11:24 -0600

    cambios en secion de pruebas

--- test/unidad/configuracionesDom.js
+++ test/unidad/configuracionesDom.js
@@ -1,19 +1,25 @@
 import { JSDOM } from "jsdom";
 
-// Creamos un DOM simulado
-
 const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`, {
-    url: "http://localhost", // ✅ Esto habilita localStorage
+    url: "http://localhost",
     pretendToBeVisual: true,
 });
 
-// Exponemos los objetos globales que necesitan los componentes
 global.window = dom.window;
 global.document = dom.window.document;
-global.navigator = dom.window.navigator;
-global.HTMLElement = dom.window.HTMLElement;
-global.customElements = dom.window.customElements;
-global.CustomEvent = dom.window.CustomEvent;
-global.localStorage = dom.window.localStorage;
 
+// NO reasignar global.navigator, solo usar el del dom
+// Si necesitas modificar userAgent, hazlo así:
+Object.defineProperty(global.window.navigator, 'userAgent', {
+    value: 'node.js',
+    configurable: true,
+    writable: false,
+    enumerable: true
+});
 
+global.navigator = global.window.navigator;
+
+global.HTMLElement = global.window.HTMLElement;
+global.customElements = global.window.customElements;
+global.CustomEvent = global.window.CustomEvent;
+global.localStorage = global.window.localStorage;
